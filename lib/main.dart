520d140b8ad0b01f6d8b9420f1710887b96293aa
gwanhyeong <wudy343@gmail.com>
2021-09-21 20:34:07 +0900

    feat: BottomNavigationBar (뱃지 포함)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_netflix_clone/widget/custom_app_bar.dart';
+import 'package:flutter_netflix_clone/widget/custom_bottom_navigation_bar.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const menuItems = [
+    {
+      'name': '홈',
+      'icon': Icons.home_rounded,
+      'badge': false,
+    },
+    {
+      'name': '공개 예정',
+      'icon': Icons.video_library_rounded,
+      'badge': true,
+    },
+    {
+      'name': '저장한 컨텐츠 목록',
+      'icon': Icons.download_for_offline_rounded,
+      'badge': false,
+    },
+  ];
+
   const MyApp({Key? key}) : super(key: key);
 
   @override
@@ -17,6 +36,9 @@ class MyApp extends StatelessWidget {
         extendBodyBehindAppBar: true, // body height에 AppBar height도 포함시킴
         body: Container(
           color: Colors.black,
+        ),
+        bottomNavigationBar: const CustomBottomNavigationBar(
+          items: menuItems,
         ),
       ),
     );
---
+++ lib/widget/custom_bottom_navigation_bar.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final List items;
+  const CustomBottomNavigationBar({Key? key, required this.items})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: generateItems(),
+      elevation: 0.0,
+      backgroundColor: const Color(0xff121212),
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.grey[800],
+    );
+  }
+
+  List<BottomNavigationBarItem> generateItems() {
+    return items.map((item) {
+      return BottomNavigationBarItem(
+        label: item['name'],
+        icon: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 0),
+              child: Icon(item['icon']),
+            ),
+            if (item['badge'])
+              Positioned(
+                right: 0,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.all(4.0),
+                    child: Text(
+                      '1',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      );
+    }).toList();
+  }
+}
